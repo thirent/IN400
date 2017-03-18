da34f273ee2e1d29467f79f4a32a5e51cc1b3191
pepnou <pepnou1@gmail.com>
2017-03-18 11:50:57 +0100

    oui

--- projet_algo/creation_ligne/ligne.c
+++ projet_algo/creation_ligne/ligne.c
@@ -1,36 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #define nbr_station 376
 #define t_max 200
 
-/*FILE* copie_fichier_metro()
+int nbr_terminus = nbr_station;
+
+int meme_gare(int n1,int n2)
 {
-	char buf[1024];
-	int reste;
-	
 	FILE* metro = fopen("metro.txt","r");
-	FILE* metro_c = fopen("metro_copie.txt","w+");
 	
-	while((reste = fread(buf,1,1024,metro)) != 0)
-		fwrite(buf,1,reste,metro_c);
+	char buf[t_max];
+	char nom[t_max],nom1[t_max],nom2[t_max];
+	char tmp;
+	int num;
+	
+	while(fgets(buf,t_max,metro) != NULL)
+	{
+		if(buf[0] == 'V')
+		{
+			sscanf(buf,"%c %d %s",&tmp,&num,nom);
+			
+			if(num == n1)
+			{
+				//strcpy(nom1,nom);
+				strcpy(nom1,&buf[1+1+4+1]);
+			}
+			if(num == n2)
+			{
+				//strcpy(nom2,nom);
+				strcpy(nom2,&buf[1+1+4+1]);
+			}
+		}
+	}
 	
 	fclose(metro);
 	
-	return metro_c;
-}*/
+	if(strcmp(nom1,nom2) == 0)return 1;
+	else return 0;
+}
 
-int main()
+int* terminus()
 {
-	
-	FILE* ligne = fopen("resultat_ligne.txt","w+");
 	FILE* metro = fopen("metro.txt","r");
-	//FILE* metro_c = copie_fichier_metro();
 	
 	int i,j,k,l;
 	int station[nbr_station];
-	int nbr_terminus = nbr_station;
 	char buf[t_max];
 	char c;
 	
@@ -43,19 +60,16 @@ int main()
 			if(buf[0] == 'E')
 			{
 				sscanf(buf,"%c %d %d %d",&c,&j,&k,&l);
-				//printf("%d %d\n",j,k);
-				if(((i == j)||(i == k))&&((l<180)||(l==287)))station[i]++;
+				if(((i == j)||(i == k))&&(!meme_gare(j,k)))station[i]++;
 			}
 		}
 		if(station[i] > 1)nbr_terminus--;
 		fseek(metro,0,SEEK_SET);
 	}
 	
-	int terminus[nbr_terminus];
+	int* terminus = malloc(nbr_terminus*sizeof(int));
 	
-	j = 0;
-	
-	for(i=0;i<nbr_station;i++)
+	for(j=0,i=0;i<nbr_station;i++)
 	{
 		if(station[i] == 1)
 		{
@@ -65,14 +79,68 @@ int main()
 		}
 	}
 	
+	fclose(metro);
+	
+	return terminus;
+}
+
+int prochain_arret(int* traite,int arret)
+{
+	FILE* metro = fopen("metro.txt","r");
+	
+	int p_arret;
+	
+	traite[arret] = 1;
 	
 	
 	
+	char buf[t_max];
+	char nom[t_max],nom1[t_max],nom2[t_max];
+	char c;
+	int j,k,l;
+	
+	while(fgets(buf,t_max,metro) != NULL)
+	{
+		if(buf[0] == 'E')
+		{
+			sscanf(buf,"%c %d %d %d",&c,&j,&k,&l);
+			
+			
+		}
+	}
 	
 	fclose(metro);
-	fclose(ligne);
+}
+
+void creation_ligne(int* terminus)
+{
+	FILE* ligne = fopen("resultat_ligne.txt","w+");
 	
-	//system("rm metro_copie.txt");
+	int traite[nbr_station];
+	int i;
+	
+	for(i=0;i<nbr_station;i++)traite[i] = 0;
+	
+	for(i=0;i<nbr_terminus;i++,p_arret=0)
+	{
+		if(!traite[terminus[i]])
+		{
+			fprintf(ligne,"%d ",terminus[i]);
+			p_arret = prochain_arret(traite,terminus[i]);
+			while(p_arret != -1)
+			{
+				fprintf(ligne,"%d ",p_arret);
+			}
+		}
+	}
+	
+	fclose(ligne);
+}
+
+int main()
+{
+	terminus();
+	
 	
 	exit(0);
 }
