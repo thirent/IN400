6ae1d8748d022294a08cf86299997fce5834a9c4
pepnou <pepnou1@gmail.com>
2017-03-31 13:37:53 +0200

    oui

--- projet_os/cesar.c
+++ projet_os/cesar.c
@@ -21,7 +21,7 @@ int nombre_ligne(char* path)
 	
 	int res=0;char c;
 	
-	while((c = read(fd,&c,sizeof(char))) != 0)
+	while(read(fd,&c,sizeof(char)) != 0)
 	{
 		if(c == '\n') res++;
 	}
@@ -32,7 +32,6 @@ int nombre_ligne(char* path)
 instruction* recup_inst(char* nom_fic,int nbr_ligne)
 {
 	int fd = open(nom_fic,O_RDONLY);
-	//int nbr_ligne = nombre_ligne(&fd);
 	int i=0;
 	char c;
 	
@@ -49,20 +48,20 @@ instruction* recup_inst(char* nom_fic,int nbr_ligne)
 		pos = 0;
 		signe = 1;
 		
-		while((!read(fd,&c,sizeof(char)))&&(c != ';'))
+		while((read(fd,&c,sizeof(char)) != 0) && (c != ';'))
 		{
 			inst[i].path[pos] = c;
 			pos++;
 			taille++;
 			if(taille == taille_max-1)
 			{
-				inst[i].path = realloc(inst[i].path,taille_max*sizeof(char)*2);
 				taille_max *= 2;
+				inst[i].path = realloc(inst[i].path,taille_max*sizeof(char));
 			}
 		}
 		inst[i].path[pos] = '\0';
-		
-		while((!read(fd,&c,sizeof(char)))&&(c != ';'))
+				
+		while((read(fd,&c,sizeof(char)) != 0) && (c != ';'))
 		{
 			if(c == '-') signe = -1;
 			else inst[i].decalage = (inst[i].decalage*10) + (c - '0');
@@ -90,16 +89,16 @@ int main(int argc, char** argv)
 	int pipes[nbr_ligne][2];
 	
 	char buf[tmax];
-	
+		
 	for(i=0;i<nbr_ligne;i++)
 	{
 		pipe(pipes[i]);
 		
-		printf("%s %d %c",inst[i].path,inst[i].decalage,inst[i].sens);
+		printf("%s %d %c\n",inst[i].path,inst[i].decalage,inst[i].sens);
 		
 		pid[i] = fork();
 		if(pid[i] == 0) goto fin_boucle;
-	}
+	}	
 	fin_boucle:
 	
 	if(i == nbr_ligne) //parent
